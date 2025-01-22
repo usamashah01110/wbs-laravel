8af395d10fc5f44ee054d5dba50f5085f64a2738
Fahad <fahadurrehman001@gmail.com>
2025-01-22 09:28:03 +0500

    update user detail page

--- resources/views/user-details.blade.php
+++ resources/views/user-details.blade.php
@@ -70,12 +70,12 @@
         <p class="recipient-email text-sm text-gray-600">{{ $recipient->email ?? 'N/A' }}</p>
       </div>
       <div class="space-x-2">
-        <!--<button class="text-blue-500" onclick="openattPopup('willRecipients', {{ $recipient->id }})">-->
-        <!--  <i class="fas fa-edit"></i>-->
-        <!--</button>-->
-        <!--<button class="text-red-500" onclick="deleteattRecipient('willRecipients', {{ $recipient->id }})">-->
-        <!--  <i class="fas fa-trash-alt"></i>-->
-        <!--</button>-->
+        <button class="text-blue-500" onclick="openattPopup('willRecipients', {{ $recipient->id }})">
+         <i class="fas fa-edit"></i>
+        </button>
+        <button class="text-red-500" onclick="deleteattRecipient('willRecipients', {{ $recipient->id }})">
+         <i class="fas fa-trash-alt"></i>
+        </button>
       </div>
     @empty
       <p class="text-gray-500">N/A</p>
@@ -107,12 +107,12 @@
         <p class="recipient-email text-sm text-gray-600">{{ $recipient->email ?? 'N/A' }}</p>
       </div>
       <div class="space-x-2">
-        <!--<button class="text-blue-500" onclick="openattPopup('willRecipients', {{ $recipient->id }})">-->
-        <!--  <i class="fas fa-edit"></i>-->
-        <!--</button>-->
-        <!--<button class="text-red-500" onclick="deleteattRecipient('willRecipients', {{ $recipient->id }})">-->
-        <!--  <i class="fas fa-trash-alt"></i>-->
-        <!--</button>-->
+        <button class="text-blue-500" onclick="openattPopup('willRecipients', {{ $recipient->id }})">
+         <i class="fas fa-edit"></i>
+        </button>
+        <button class="text-red-500" onclick="deleteattRecipient('willRecipients', {{ $recipient->id }})">
+         <i class="fas fa-trash-alt"></i>
+        </button>
       </div>
     @empty
       <p class="text-gray-500">N/A</p>
