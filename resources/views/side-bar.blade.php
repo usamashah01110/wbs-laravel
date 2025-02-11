a53b03e103c7f8ad5deb40c5b9c1c2f8f7f3bcd0
Fahad <fahadurrehman001@gmail.com>
2025-02-12 01:57:59 +0500

    fixes

--- resources/views/side-bar.blade.php
+++ resources/views/side-bar.blade.php
@@ -1,26 +1,24 @@
 <nav id="sidebar"
     class="bg-[#415a77] text-gray-100 w-64 h-full flex-shrink-0 transition-all duration-300 flex flex-col justify-between">
     <div>
-        <div class="p-4">
-            <button id="sidebarToggle" class="text-white hover:scale-110">
-                <i id="toggleArrow" class="fas fa-arrow-left"></i>
-            </button>
-        </div>
+        <button id="sidebarToggle" class="text-white hover:scale-110 transition-all duration-300 p-4">
+            <i id="toggleArrow" class="fas fa-arrow-left"></i>
+        </button>
         <ul>
             <a href="{{ route('admin.dashboard') }}">
-                <li id="dashboardLink" class="hover:bg-gray-700 px-4 py-2 flex items-center gap-4 cursor-pointer">
+                <li id="dashboardLink" class="hover:bg-gray-700 px-4 py-2 flex items-center gap-4 cursor-pointer transition-all duration-300">
                     <i class="fas fa-tachometer-alt text-green-500"></i>
                     <span class="sidebar-text">Dashboard</span>
                 </li>
             </a>
             <a href="{{ route('admin.profile') }}">
-                <li id="myAccountLink" class="hover:bg-gray-700 px-4 py-2 flex items-center gap-4 cursor-pointer">
-                    <i class="fas fa-user-circle text-blue-500-"></i>
+                <li id="myAccountLink" class="hover:bg-gray-700 px-4 py-2 flex items-center gap-4 cursor-pointer transition-all duration-300">
+                    <i class="fas fa-user-circle text-blue-500"></i>
                     <span class="sidebar-text">My Account</span>
                 </li>
             </a>
             <form method="POST" action="{{ route('logout') }}">
-                <li id="logoutLink" class="hover:bg-gray-700 px-4 py-2 flex items-center gap-4 cursor-pointer">
+                <li id="logoutLink" class="hover:bg-gray-700 px-4 py-2 flex items-center gap-4 cursor-pointer transition-all duration-300">
                     <i class="fas fa-sign-out-alt text-red-500"></i>
                     <span class="sidebar-text">
                         @csrf
