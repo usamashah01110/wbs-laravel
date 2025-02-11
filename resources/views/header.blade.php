f22cf5ccec0c576e5ecf979dc7a69c814843d415
Fahad <fahadurrehman001@gmail.com>
2025-02-12 01:45:45 +0500

    fixes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <header class="bg-[#f47d61] shadow-lg px-6 py-4 flex justify-between items-center">
     <div class="flex items-center">
-        <img src="{{ asset('images/WBS-Logo.png') }}" alt="Logo" class="h-24" />
+        <img src="{{ asset('images/WBS-Logo.png') }}" alt="Logo" class="h-14" />
     </div>
     <div class="flex items-center gap-4">
         <div>
@@ -12,6 +12,6 @@
             </span>
         </div>
         <img src="{{ auth()->user() && auth()->user()->profile_image ? asset('storage/' . auth()->user()->profile_image) : asset('images/user.png') }}"
-            alt="Profile" class="w-8 h-8 rounded-full" />
+            alt="Profile" class="w-14 h-14 rounded-full" />
     </div>
 </header>
