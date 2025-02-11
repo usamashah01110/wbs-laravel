f4989095bca931624e69116785d90a19dfeee85f
Fahad <fahadurrehman001@gmail.com>
2025-02-12 02:04:35 +0500

    fixes

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -78,7 +78,7 @@ const toggleArrow = document.getElementById("toggleArrow");
 const sidebarText = document.querySelectorAll(".sidebar-text");
 
 sidebarToggle.addEventListener("click", () => {
-  sidebar.classList.toggle("w-64");
+  sidebar.classList.toggle("w-48");
   sidebar.classList.toggle("w-16");
   toggleArrow.classList.toggle("rotate-180");
   sidebarText.forEach((text) => text.classList.toggle("hidden"));
