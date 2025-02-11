7dd98e686f8f40b959ad91788871f9b0e8057049
Fahad <fahadurrehman001@gmail.com>
2025-02-12 01:44:58 +0500

    fixes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,26 +1,17 @@
-<header
-    class="bg-[#f47d61] shadow-lg px-6 py-4 flex justify-between items-center"
->
+<header class="bg-[#f47d61] shadow-lg px-6 py-4 flex justify-between items-center">
     <div class="flex items-center">
-        <img
-            src="{{ asset('images/WBS-Logo.png') }}"
-            alt="Logo"
-            class="h-14"
-        />
+        <img src="{{ asset('images/WBS-Logo.png') }}" alt="Logo" class="h-24" />
     </div>
     <div class="flex items-center gap-4">
-        <span class="text-gray-100 font-medium hidden md:block">
-            Welcome Back!
-        </span>
-        <div class="flex items-center gap-2">
-            <img
-                src="{{ auth()->user() && auth()->user()->profile_image ? asset('storage/' . auth()->user()->profile_image) : asset('images/user.png') }}"
-                alt="Profile"
-                class="w-8 h-8 rounded-full"
-            />
+        <div>
+            <p class="text-gray-100 font-medium hidden md:block">
+                Welcome Back!
+            </p>
             <span class="font-bold text-white">
                 {{ Auth::check() ? Auth::user()->firstname . ' ' . Auth::user()->lastname : 'Guest' }}
             </span>
         </div>
+        <img src="{{ auth()->user() && auth()->user()->profile_image ? asset('storage/' . auth()->user()->profile_image) : asset('images/user.png') }}"
+            alt="Profile" class="w-8 h-8 rounded-full" />
     </div>
 </header>
