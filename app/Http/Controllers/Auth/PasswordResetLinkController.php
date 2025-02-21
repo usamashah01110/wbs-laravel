ace0d08afcd4ad8b87062f3c9ea3b313471568c1
Axon Technologies <axontechnologies@Axons-MacBook-Air.local>
2025-02-22 02:51:20 +0500

    fix forget pass

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -1,28 +1,44 @@
 <?php
 
-namespace App\Mail;
+namespace App\Http\Controllers\Auth;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\View\View;
 
-class PasswordResetLinkController extends Mailable
+class PasswordResetLinkController extends Controller
 {
-    use Queueable, SerializesModels;
-
-    public $user;
-    public $token;
-
-    public function __construct($user, $token)
+    /**
+     * Display the password reset link request view.
+     */
+    public function create(): View
     {
-        $this->user = $user;
-        $this->token = $token;
+        return view('auth.forgot-password');
     }
 
-    public function build()
+    /**
+     * Handle an incoming password reset link request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
     {
-        return $this->subject('Password Reset Link')
-            ->view('emails.reset-link'); 
+        $request->validate([
+            'email' => ['required', 'email'],
+        ]);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status == Password::RESET_LINK_SENT
+            ? back()->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
