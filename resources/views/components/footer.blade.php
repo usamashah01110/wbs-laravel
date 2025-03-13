f4f6573092424abfa2cf616f04ae098bae81c700
fahad <fahadurrehman001@gmail.com>
2025-03-14 03:00:55 +0500

    add social links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -38,7 +38,7 @@
                     <!-- <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                     </svg> -->
-                    <img class="h-8 w-8" src="../../../public/images/facebook.png" alt="facebook icon">
+        <img src="{{ asset('images/facebook.png') }}" alt="facebook icon" class="h-8" />
                 </a>
 
                 <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] cursor-pointer transition-all duration-300" href="https://www.instagram.com/willbesent_/?fbclid=IwZXh0bgNhZW0CMTEAAR2y1wetXP85EVmW4o8rXolE1E-WjeOr2iP2pK_KvYXXQQh9Io8kYOutz_g_aem_7RG-SDtXkzhWr3ga46I3fw">
