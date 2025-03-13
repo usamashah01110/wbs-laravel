098b60ed20cfec1c3152d5f25300b8b1dbd975e0
fahad <fahadurrehman001@gmail.com>
2025-03-14 02:39:01 +0500

    add social links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -34,16 +34,16 @@
             </p>
 
             <div class="flex gap-2">
-                <a class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] transition-all duration-300" href="#">
+                <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] transition-all duration-300" href="https://www.facebook.com/profile.php?id=61571193345238">
                     <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                     </svg>
                 </a>
 
-                <a class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] transition-all duration-300" href="#">
-                    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                    </svg>
+                <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] transition-all duration-300" href="https://www.instagram.com/willbesent_/?fbclid=IwZXh0bgNhZW0CMTEAAR2y1wetXP85EVmW4o8rXolE1E-WjeOr2iP2pK_KvYXXQQh9Io8kYOutz_g_aem_7RG-SDtXkzhWr3ga46I3fw">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50">
+                      <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
+                </svg>
                 </a>
             </div>
         </div>
