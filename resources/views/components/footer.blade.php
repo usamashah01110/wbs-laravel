4fac16def6cd0bd818b335844c0805d1c416ffe6
fahad <fahadurrehman001@gmail.com>
2025-03-14 02:57:24 +0500

    add social links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -35,13 +35,14 @@
 
             <div class="flex gap-2">
                 <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] transition-all duration-300" href="https://www.facebook.com/profile.php?id=61571193345238">
-                    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <!-- <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                    </svg>
+                    </svg> -->
+                    <img class="h-8 w-8" src="../../../public/images/facebook.com" alt="facebook icon">
                 </a>
 
                 <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] cursor-pointer transition-all duration-300" href="https://www.instagram.com/willbesent_/?fbclid=IwZXh0bgNhZW0CMTEAAR2y1wetXP85EVmW4o8rXolE1E-WjeOr2iP2pK_KvYXXQQh9Io8kYOutz_g_aem_7RG-SDtXkzhWr3ga46I3fw">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0,0,256,256">
+                <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0,0,256,256">
 <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M16,3c-7.17,0 -13,5.83 -13,13v18c0,7.17 5.83,13 13,13h18c7.17,0 13,-5.83 13,-13v-18c0,-7.17 -5.83,-13 -13,-13zM37,11c1.1,0 2,0.9 2,2c0,1.1 -0.9,2 -2,2c-1.1,0 -2,-0.9 -2,-2c0,-1.1 0.9,-2 2,-2zM25,14c6.07,0 11,4.93 11,11c0,6.07 -4.93,11 -11,11c-6.07,0 -11,-4.93 -11,-11c0,-6.07 4.93,-11 11,-11zM25,16c-4.96,0 -9,4.04 -9,9c0,4.96 4.04,9 9,9c4.96,0 9,-4.04 9,-9c0,-4.96 -4.04,-9 -9,-9z"></path></g></g>
 </svg>
                 </a>
