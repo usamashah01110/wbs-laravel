2c25621ed36b289658860df28a86975cf21e0ea0
fahad <fahadurrehman001@gmail.com>
2025-03-14 03:10:53 +0500

    add social links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -33,16 +33,17 @@
                 Copyright &copy; <script>document.write(new Date().getFullYear())</script> Will Be Sent. All Rights Reserved. 
             </p>
 
-            <div class="flex gap-2">
+            <div class="flex gap-2 mr-8">
+            <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] cursor-pointer transition-all duration-300" href="https://www.instagram.com/willbesent_/?fbclid=IwZXh0bgNhZW0CMTEAAR2y1wetXP85EVmW4o8rXolE1E-WjeOr2iP2pK_KvYXXQQh9Io8kYOutz_g_aem_7RG-SDtXkzhWr3ga46I3fw">
+               
+               <img src="{{ asset('images/instagram.png') }}" alt="facebook icon" class="h-6" />
+                       </a>
                 <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] transition-all duration-300" href="https://www.facebook.com/profile.php?id=61571193345238">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+                   
+        <img src="{{ asset('images/facebook.png') }}" alt="facebook icon" class="h-6" />
                 </a>
 
-                <a  target="blank" class="p-2 text-sm font-semibold rounded-md text-white hover:bg-[#415a77] cursor-pointer transition-all duration-300" href="https://www.instagram.com/willbesent_/?fbclid=IwZXh0bgNhZW0CMTEAAR2y1wetXP85EVmW4o8rXolE1E-WjeOr2iP2pK_KvYXXQQh9Io8kYOutz_g_aem_7RG-SDtXkzhWr3ga46I3fw">
-                <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0,0,256,256">
-<g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M16,3c-7.17,0 -13,5.83 -13,13v18c0,7.17 5.83,13 13,13h18c7.17,0 13,-5.83 13,-13v-18c0,-7.17 -5.83,-13 -13,-13zM37,11c1.1,0 2,0.9 2,2c0,1.1 -0.9,2 -2,2c-1.1,0 -2,-0.9 -2,-2c0,-1.1 0.9,-2 2,-2zM25,14c6.07,0 11,4.93 11,11c0,6.07 -4.93,11 -11,11c-6.07,0 -11,-4.93 -11,-11c0,-6.07 4.93,-11 11,-11zM25,16c-4.96,0 -9,4.04 -9,9c0,4.96 4.04,9 9,9c4.96,0 9,-4.04 9,-9c0,-4.96 -4.04,-9 -9,-9z"></path></g></g>
-</svg>
-                </a>
+                
             </div>
         </div>
     </div>
